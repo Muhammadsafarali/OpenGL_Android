2e5cec108e52ac1c56b85cf1adfb87fce7fcac2e
Muhammadsafarali <dr.safarali@yandex.ru>
2017-06-30 16:57:58 +0300

    Урок 169. OpenGL. Шейдеры.

---
+++ app/src/main/java/com/dublick/opengl_01/OpenGLRenderer.java
@@ -0,0 +1,83 @@
+package com.dublick.opengl_01;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView.Renderer;
+
+import com.dublick.opengl_01.Utils.ShaderUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by 3dium on 30.06.2017.
+ */
+
+public class OpenGLRenderer implements Renderer {
+
+    private Context context;
+    private int programId;
+    private FloatBuffer vertexData;
+    private int uColorLocation;
+    private int aPositionLocation;
+
+    public OpenGLRenderer(Context context) {
+        this.context = context;
+        prepareData();
+    }
+
+    private void prepareData() {
+        float[] vertices = {-0.5f, -0.2f, 0.0f, 0.2f, 0.5f, -0.2f,};
+        vertexData = ByteBuffer.allocateDirect(vertices.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+        vertexData.put(vertices);
+    }
+
+    private void bindData() {
+        uColorLocation = glGetUniformLocation(programId, "u_Color");
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        aPositionLocation = glGetAttribLocation(programId, "a_Position");
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, 2, GL_FLOAT, false, 0, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0,0,width,height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(0.2f, 0.2f, 0.2f, 1f);
+        int vertexShaderId = ShaderUtils.createShader(context, GL_VERTEX_SHADER, R.raw.vertex_shader);
+        int fragmentShaderId = ShaderUtils.createShader(context, GL_FRAGMENT_SHADER, R.raw.fragment_shader);
+        programId = ShaderUtils.createProgram(vertexShaderId, fragmentShaderId);
+        glUseProgram(programId);
+        bindData();
+    }
+}
